4b6e4ceb370cd553021cfadd5a768667459ceb3a
Feanor <hamzaaygan1995@gmail.com>
2023-03-27 22:58:46 +0300

    project starting files

---
+++ components/InputField.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="input-field">
+    <input type="text" name="city" id="city" placeholder="Search Location" />
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.input-field {
+  /* width: 50%; */
+  display: flex;
+  flex-direction: column;
+}
+
+.input-field input {
+  width: 20rem;
+  padding: 0.5rem;
+  border-radius: 10px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-container">
+    <InputField />
+    <!-- <Tutorial /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
+
+<style>
+.app-container {
+  border: 1px solid red;
+  height: 95vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* background-image: url("https://picsum.photos/1600/900"); */
+}
+</style>
